257c7cc65c12e78de74ee5b9575444360c98d8c1
Nikolay <nikolay.p@it-dimension.com>
2023-01-26 19:05:54 +0200

    deploy test_app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -17,9 +17,14 @@ function App() {
         >
           Learn React
         </a>
+        <div>TEST APP</div>
+        <div>TEST APP</div>
+        <div>TEST APP</div>
+        <div>TEST APP</div>
+        <div>TEST APP</div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
